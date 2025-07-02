8c101bc3935d25da94322d018088565462649d1f
Dhalguin Hernandez <dhalguinjesus21nov@gmail.com>
2025-07-01 21:33:33 -0400

    fix: camel case icon

--- packages/icons/dotIcon/index.tsx
+++ packages/icons/dotIcon/index.tsx
@@ -9,9 +9,9 @@ export const DotIcon: React.FC<IconProps> = ({ width, height, fill }) => {
       viewBox="0 0 24 24"
       fill="none"
       stroke={fill}
-      stroke-width="4"
-      stroke-linecap="round"
-      stroke-linejoin="round"
+      strokeWidth="4"
+      strokeLinecap="round"
+      strokeLinejoin="round"
     >
       <circle cx="12.1" cy="12.1" r="1" />
     </svg>
